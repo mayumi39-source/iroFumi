a658ddfd0d99307965e0d4b1e8b5cf2d66956343
mayumi39-source <mayumi.39102926@gmail.com>
2026-04-18 20:24:37 +0900

    フッター・利用規約・プライバシーポリシーページの追加

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,10 @@ class PagesController < ApplicationController
   def top
     redirect_to home_path if user_signed_in?
   end
+
+  def terms
+  end
+
+  def privacy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   root to: "pages#top"
   get "home", to: "color_entries#index", as: :home
   get "color_entries/complete", to: "color_entries#complete", as: :complete_color_entry
+  get "terms", to: "pages#terms", as: :terms
+  get "privacy", to: "pages#privacy", as: :privacy
   resources :color_entries, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end
