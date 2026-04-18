133b69ba845168bea44a00e59a16d2e3a5a55854
mayumi39-source <mayumi.39102926@gmail.com>
2026-04-18 14:12:42 +0900

    色投稿機能・カレンダー表示の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  get 'color_entries/index'
-  get 'color_entries/new'
-  get 'color_entries/create'
-  get 'color_entries/edit'
-  get 'color_entries/update'
-  get 'color_entries/destroy'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "color_entries#index"
+  resources :color_entries, only: [:index, :new, :create, :edit, :update, :destroy]
 end
