e131d1b5bfbede911af69827f80c715cf3345ebf
mayumi39-source <mayumi.39102926@gmail.com>
2026-04-18 21:07:07 +0900

    作業中の変更を保存

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
   def top
-    redirect_to home_path if user_signed_in?
-    render layout: false
+    if user_signed_in?
+      redirect_to home_path
+    else
+      render layout: false
+    end
   end
 
   def terms
