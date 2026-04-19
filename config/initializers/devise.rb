4146754c74cfead737c527f3a539b55c0cde65f8
mayumi39-source <mayumi.39102926@gmail.com>
2026-04-19 11:00:23 +0900

    flashメッセージが表示されない問題を修正

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -270,6 +270,7 @@ Devise.setup do |config|
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
+  config.responder.flash_keys = [:notice, :alert]
 
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
